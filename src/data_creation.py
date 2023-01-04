79cbf375c6351b162cf49654cea2455752f6ce83
mtseng15 <tseng.micah@gmail.com>
2023-01-04 15:48:16 -0600

    updated data creation

--- src/data_creation.py
+++ src/data_creation.py
@@ -7,8 +7,9 @@ size = 1024*1024
 
 for i in range(files):
     filename = ''.join([random.choice(string.ascii_letters) for i in range(10)])
+    extension = ''.join([random.choice(string.ascii_letters) for i in range(3)])
     chars = ''.join([random.choice(string.printable) for i in range(size)])
-    with open("data/"+filename+'.txt', 'w') as f:
+    with open("data/"+filename+"."+extension, 'w') as f:
         f.write(chars)
 
 
